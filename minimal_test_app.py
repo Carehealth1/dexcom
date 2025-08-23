d8dfbb930e9a2b8748ed8efd8dadcac17ddc740c
Chaitanya-CareHealth <107814464+Carehealth1@users.noreply.github.com>
2025-08-22 20:14:21 -0500

    Add files via upload

---
+++ minimal_test_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("🩺 Dexcom Data Viewer - Test Version")
+
+st.write("If you can see this, the basic app is working!")
+
+# Test basic functionality
+if st.button("Test Button"):
+    st.success("Button works!")
+
+st.write("App is running successfully.")
